010a8f4866ffb4e6eda489264981cb46f78bf796
Ella <efb4518@ic.ac.uk>
2022-01-15 22:13:37 +0000

    Edit data loader for peg dataset

--- data_loader.py
+++ data_loader.py
@@ -5,9 +5,12 @@ from PIL import Image
 
 
 class DataLoader:
-    def __init__(self, dataset_name, img_res=(128, 128)):
+    def __init__(self, dataset_name, img_res=(512, 512)):
         self.dataset_name = dataset_name
         self.img_res = img_res
+        # self.dataset_size = 29961
+        self.dataset_size = 1000
+        self.randomisations = 5
 
         # Convert to Lab colourspace
         # srgb_p = ImageCms.createProfile("sRGB")
@@ -16,34 +19,39 @@ class DataLoader:
         # self.rgb2lab = ImageCms.buildTransformFromOpenProfiles(srgb_p, lab_p, "RGB", "LAB")
         # self.lab2rgb = ImageCms.buildTransformFromOpenProfiles(lab_p, srgb_p, "LAB", "RGB")
 
-    def load_data(self, batch_size=1, is_testing=False):
-        data_type = "train" if not is_testing else "test"
-        path = glob('./datasets/%s/%s/*' % (self.dataset_name, data_type))
+    def load_data(self, batch_size=1):
+        img_nums = np.random.randint(self.dataset_size, size=batch_size)
 
-        random.shuffle(path)
-        batch_images = np.random.choice(path, size=batch_size)
+        # path = glob('./datasets/%s/%s/*' % (self.dataset_name))
+        # batch_images = np.random.choice(path, size=batch_size)
 
         imgs_A = []
         imgs_B = []
-        for img_path in batch_images:
-            img = self.imread(img_path)
+        for img_num in img_nums:
+            randomisation = np.random.randint(self.randomisations)
+            img_path_randomised = glob(
+                'datasets/%s/different_randomisations/%s/RGB/11%s.png' % (self.dataset_name, randomisation, img_num))
+            img_path_target = glob(
+                'datasets/%s/reference_env/0/RGB/11%s.png' % (self.dataset_name, img_num))
+            img_randomised = self.imread(img_path_randomised[0])
+            img_target = self.imread(img_path_target[0])
 
-            h, w, _ = img.shape
-            _w = int(w / 2)
-            img_A, img_B = img[:, :_w, :], img[:, _w:, :]
+            # h, w, _ = img.shape
+            # _w = int(w / 2)
+            # img_A, img_B = img[:, :_w, :], img[:, _w:, :]
 
-            img_A = np.array(Image.fromarray(img_A).resize(self.img_res))
+            img_target = np.array(Image.fromarray(img_target).resize(self.img_res))
             # img_A = scipy.misc.imresize(img_A, self.img_res)
             # img_B = scipy.misc.imresize(img_B, self.img_res)
-            img_B = np.array(Image.fromarray(img_B).resize(self.img_res))
+            img_randomised = np.array(Image.fromarray(img_randomised).resize(self.img_res))
 
             # If training => do random flip
-            if not is_testing and np.random.random() < 0.5:
-                img_A = np.fliplr(img_A)
-                img_B = np.fliplr(img_B)
+            # if not is_testing and np.random.random() < 0.5:
+            #     img_A = np.fliplr(img_A)
+            #     img_B = np.fliplr(img_B)
 
-            imgs_A.append(img_A)
-            imgs_B.append(img_B)
+            imgs_A.append(img_target)
+            imgs_B.append(img_randomised)
 
         imgs_A = np.array(imgs_A) / 127.5 - 1.
         imgs_B = np.array(imgs_B) / 127.5 - 1.
@@ -51,34 +59,48 @@ class DataLoader:
         return imgs_A, imgs_B
 
     def load_batch(self, batch_size=1, is_testing=False):
-        data_type = "train" if not is_testing else "val"
-        path = glob('./datasets/%s/%s/*' % (self.dataset_name, data_type))
-        random.shuffle(path)
+        # data_type = "train" if not is_testing else "val"
+        # path = glob('./datasets/%s/%s/*' % (self.dataset_name, data_type))
+        # random.shuffle(path)
 
-        self.n_batches = int(len(path) / batch_size)
+        self.n_batches = int(self.dataset_size / batch_size)
 
         for i in range(self.n_batches - 1):
-            batch = path[i * batch_size:(i + 1) * batch_size]
-            imgs_A, imgs_B = [], []
-            for img in batch:
-                img = self.imread(img)
-                h, w, _ = img.shape
+            img_nums = np.random.randint(self.dataset_size, size=batch_size)
 
-                half_w = int(w / 2)
-                img_A = img[:, :half_w, :]
-                img_B = img[:, half_w:, :]
+            # path = glob('./datasets/%s/%s/*' % (self.dataset_name))
+            # batch_images = np.random.choice(path, size=batch_size)
 
-                img_A = np.array(Image.fromarray(img_A).resize(self.img_res))
+            imgs_A = []
+            imgs_B = []
+            for img_num in img_nums:
+                randomisation = np.random.randint(self.randomisations)
+                img_path_randomised = glob('datasets/%s/different_randomisations/%s/RGB/11%s.png' % (
+                    self.dataset_name, randomisation, img_num))
+                # print(img_path_randomised)
+                # print(img_num, randomisation, '/datasets/%s/different_randomisations/%s/RGB/11%s.png' % (
+                #     self.dataset_name, randomisation, img_num))
+                img_path_target = glob(
+                    'datasets/%s/reference_env/0/RGB/11%s.png' % (self.dataset_name, img_num))
+                img_randomised = self.imread(img_path_randomised[0])
+                img_target = self.imread(img_path_target[0])
+
+                # h, w, _ = img.shape
+                # _w = int(w / 2)
+                # img_A, img_B = img[:, :_w, :], img[:, _w:, :]
+
+                img_target = np.array(Image.fromarray(img_target).resize(self.img_res))
                 # img_A = scipy.misc.imresize(img_A, self.img_res)
                 # img_B = scipy.misc.imresize(img_B, self.img_res)
-                img_B = np.array(Image.fromarray(img_B).resize(self.img_res))
+                img_randomised = np.array(Image.fromarray(img_randomised).resize(self.img_res))
 
-                if not is_testing and np.random.random() > 0.5:
-                    img_A = np.fliplr(img_A)
-                    img_B = np.fliplr(img_B)
+                # If training => do random flip
+                # if not is_testing and np.random.random() < 0.5:
+                #     img_A = np.fliplr(img_A)
+                #     img_B = np.fliplr(img_B)
 
-                imgs_A.append(img_A)
-                imgs_B.append(img_B)
+                imgs_A.append(img_target)
+                imgs_B.append(img_randomised)
 
             imgs_A = np.array(imgs_A) / 127.5 - 1.
             imgs_B = np.array(imgs_B) / 127.5 - 1.
