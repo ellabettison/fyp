2e2e0b2f9c4e77c398b7833e27bf6c057a03e010
Ella <efb4518@ic.ac.uk>
2022-02-15 20:26:01 +0000

    Choose randomisation params

--- scene_generator.py
+++ scene_generator.py
@@ -3,22 +3,42 @@ import numpy as np
 import random
 import glob
 from pathlib import Path
-import bpy
+# import bpy
+from PIL import Image
 
 bproc.init()
 
 
+class CanonicalParams:
+    def __init__(self):
+        self.ambient_light = 5  # 1000
+        self.camera_distance = 19
+        self.img_width = 256
+        self.img_height = 256
+
+
+class RandomisationParams:
+    def __init__(self):
+        self.min_ambient_light = 0.3
+        self.max_ambient_light = 3
+        self.min_spot_light = 0
+        self.max_spot_light = 2000
+        self.min_spot_distance = 3
+        self.max_spot_distance = 19
+
+
 class Scene:
-    def load_obj(self, obj_name, pos, rot, from_file=False, randomise_material=False):
+    def __init__(self, canonical_params, randomisation_params):
+        self.c_params = canonical_params
+        self.r_params = randomisation_params
+
+    def load_obj(self, obj_name, pos, rot, from_file=False):
         if from_file:
             obj = bproc.loader.load_obj(obj_name)[0]
         else:
             obj = bproc.object.create_primitive(obj_name)
         obj.set_location(pos)
         obj.set_rotation_euler(rot)
-
-        if randomise_material:
-            self.randomise_material(obj)
 
         return obj, bproc.object.compute_poi([obj])
 
@@ -29,14 +49,25 @@ class Scene:
 
     def set_camera(self, loc, poi):
         # Set the camera to be in front of the object
-        rotation_matrix = bproc.camera.rotation_from_forward_vec(poi - loc, inplane_rot=-2.5)
+        rotation_matrix = bproc.camera.rotation_from_forward_vec(poi - loc, inplane_rot=0)
         cam_pose = bproc.math.build_transformation_mat(loc, rotation_matrix)
         bproc.camera.add_camera_pose(cam_pose)
+        return cam_pose
 
-    def output_file(self, data):
+    def output_file(self, data, folder):
         # Write the rendering into an hdf5 file
-        bproc.writer.write_hdf5("output/", data)
-        # bproc.writer.write_bop("output/imgs/",data)
+        bproc.writer.write_hdf5("output/%s/" % folder, data)
+        arr_colours = np.array(data.get("colors")[0]).reshape((self.c_params.img_width, self.c_params.img_height, 3))
+        im = Image.fromarray(arr_colours)
+        im.save("output/%s/colours.png" % folder)
+
+        if data.__contains__("depth"):
+            arr_depth = np.array(data.get("depth")).reshape(self.c_params.img_width, self.c_params.img_height)
+            np.save("output/depths/depth.npy", arr_depth)
+
+        if data.__contains__("instance_segmaps"):
+            arr_seg = np.array(data.get("instance_segmaps")).reshape(self.c_params.img_width, self.c_params.img_height)
+            np.save("output/segmaps/segmap.npy", arr_seg)
 
     def generate_random_pos(self, height, min, max):
         return [np.random.randint(min, max), height, np.random.randint(min, max)]
@@ -50,14 +81,13 @@ class Scene:
         x_pos = np.cos(az_angle) * o
         z_pos = np.sin(az_angle) * o
 
-        return x_pos, y_pos, z_pos
+        return [x_pos, y_pos, z_pos+1]
 
     def randomise_material(self, obj):
-        obj.add_uv_mapping("cube", overwrite=True)
         # Find all materials
         materials = bproc.material.collect_all()
         # Collect all jpg images in the specified directory
-        images = glob.glob("C:\\Users\\ellab\\Documents\\fyp\\fyp\\textures\\textures\\obj_textures\\*.png")
+        images = glob.glob("textures/textures/obj_textures/*.png")
         for mat in materials:
             # Load one random image
             random_mat = random.choice(images)
@@ -65,37 +95,50 @@ class Scene:
             texture = bproc.loader.load_texture(random_mat)
             mat.infuse_texture(texture[0], mode='set', texture_scale=0.5)
 
-    def set_ambient_light(self, brightness):
-        ceiling, _ = self.load_obj("blender_objects/plane.obj", [0, 30, 0], [0, 0, 0], True, True)
-        bproc.lighting.light_surface([ceiling], emission_strength=brightness)
-
     def render_scene(self):
-        cube, poi = self.load_obj("CUBE", [0, 1, 0], [0, 0, 0])
+        bproc.utility.reset_keyframes()
 
-        box, _ = self.load_obj("blender_objects/box_tall2.obj", [0, 0, 0], [0, 0, 0], True, True)
+        # load canonical scene
+        cube, poi = self.load_obj("CUBE", [0, 0, 1], [0, 0, 0])
+        cube.add_uv_mapping("cube", overwrite=True)
+        box, _ = self.load_obj("blender_objects/box_tall2.obj", [0, 0, 0], [np.pi/2, 0, 0], True)
+        box.add_uv_mapping("cube", overwrite=True)
+        print("box bb: ", box.get_bound_box())
         box.set_scale((2, 2, 2))
+        ceiling, _ = self.load_obj("PLANE", [0, 0, 40], [0, 0, 0])
+        ceiling.set_scale((20,20,1))
+        print("plane bb: ", ceiling.get_bound_box())
+        bproc.lighting.light_surface([ceiling], emission_strength=self.c_params.ambient_light)
+        camera_loc = self.generate_pos_distance_from_point(self.c_params.camera_distance, np.pi / 2)
+        cam_pose = self.set_camera(camera_loc, poi)
 
-        print(box.get_bound_box())
-
-        # self.randomise_material(box)
-
-        self.set_spot_light([2, 10, 0], np.random.randint(200,700))
-        self.set_ambient_light(np.random.random()*6.0)
-
-        camera_loc = self.generate_pos_distance_from_point(15, np.pi / 2)
-
-        self.set_camera(camera_loc, poi)
         # Render the scene
-        bproc.renderer.set_output_format("PNG")
-        bproc.camera.set_intrinsics_from_blender_params(image_height=128, image_width=128)
-        bproc.renderer.enable_depth_output(activate_antialiasing=False)
+        bproc.camera.set_intrinsics_from_blender_params(image_height=self.c_params.img_height,
+                                                        image_width=self.c_params.img_width)
         bproc.renderer.set_max_amount_of_samples(30)
         data = bproc.renderer.render()
+        self.output_file(data, "canonical")
+        bproc.utility.reset_keyframes()
 
-        data.update(bproc.renderer.render_segmap(map_by=["class", "instance", "name"]))
+        # randomise scene
+        bproc.lighting.light_surface([ceiling], emission_strength=np.random.uniform(self.r_params.min_ambient_light,
+                                                                                    self.r_params.max_ambient_light))
+        self.randomise_material(box)
+        self.set_spot_light(self.generate_pos_distance_from_point(np.random.uniform(self.r_params.min_spot_distance,
+                                                                                    self.r_params.max_spot_distance),
+                                                                  np.pi / 2),
+                            np.random.uniform(self.r_params.min_spot_light, self.r_params.max_spot_light))
 
-        self.output_file(data)
+        # Render the scene
+        bproc.camera.add_camera_pose(cam_pose)
+        # bproc.renderer.enable_depth_output(activate_antialiasing=False)
+        data = bproc.renderer.render()
+        # data.update(bproc.renderer.render_segmap(map_by=["class", "instance", "name"]))
+        self.output_file(data, "randomised")
 
 
-scene = Scene()
+canonical_params = CanonicalParams()
+randomisation_params = RandomisationParams()
+
+scene = Scene(canonical_params, randomisation_params)
 scene.render_scene()
