2511faedfd8b329e4a470cc27bbbaf774d863999
efb4518 <efb4518@ic.ac.uk>
2022-05-29 14:06:44 +0100

    Use command line args for hyperparams

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+
+
+import blenderproc as bproc
+
+import sys
+
+sys.path.append('/vol/bitbucket/efb4518/fyp/fyp')
+
 import argparse
-from data_loader import DataLoader
 from peg_predictions_gan import RCAN
+from image_to_label import ImageToLabel
+from data_loader import DataLoader
 
 # functions_dict = {
 #     "gan": run_gan,
@@ -12,18 +21,21 @@ class Main:
         parser = argparse.ArgumentParser(description="Just an example",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-        parser.add_argument("-i", "--img_size", default=256, type=int, help="Shape of image to process")
+        parser.add_argument("-i", "--img_size", default=128, type=int, help="Shape of image to process")
         parser.add_argument("-f", "--filters", default=32, type=int, help="Filters to use for conv layers")
         parser.add_argument("-k", "--kernel_size", default=3, type=int, help="Kernel size to use for conv layers")
-        parser.add_argument("-b", "--batch_size", default=2, type=int, help="Batch size")
-        parser.add_argument("-e", "--epochs", default=10000, type=int, help="Number of epochs")
+        parser.add_argument("-b", "--batch_size", default=10, type=int, help="Batch size")
+        parser.add_argument("-e", "--epochs", default=10, type=int, help="Number of epochs")
         parser.add_argument("-d", "--dataset_name", default="/vol/bitbucket/efb4518/fyp/fyp/generated_imgs_numpy", type=str, help="Folder from which to extract canonical, randomised images etc")
         parser.add_argument("-o", "--output_folder", default="/vol/bitbucket/efb4518/fyp/fyp/generated_samples2", type=str, help="Folder in which to store examples of generated data")
-        parser.add_argument("-l", "--learning_rate", default=0.00001, type=float, help="Learning rate")
+        parser.add_argument("-l", "--learning_rate", default=0.0002, type=float, help="Learning rate")
         parser.add_argument("-s", "--use_seg", action="store_true")
         parser.add_argument("-p", "--sample_interval", default=10, type=int)
         parser.add_argument("-n", "--dataset_size", default=4500, type=int, help="How much of dataset to use for training")
         parser.add_argument("-r", "--randomisations", default=5, type=int, help="Number of randomisations to use for training")
+        parser.add_argument("-w", "--loss_weights", default=[1,1,1,0,0], nargs="+", type=int)
+        parser.add_argument("-q", "--use_depth", action="store_true")
+        parser.add_argument("-v", "--use_vector", action="store_true")
 
         args = parser.parse_args()
         # config = vars(args)
@@ -33,7 +45,15 @@ class Main:
         rcan = RCAN(config, DataLoader(config))
         rcan.train()
 
+    def run_img_to_label(self, config):
+        img_to_label = ImageToLabel(config)
+        img_to_label.train()
+
 if __name__ == "__main__":
     main_prog = Main()
     config = main_prog.get_args()
-    main_prog.run_gan(config)
+
+    print("runnnign")
+
+    cnn = ImageToLabel(config)
+    cnn.train()
