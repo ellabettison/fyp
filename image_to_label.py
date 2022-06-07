e5ab9e7bda56df93967bded0962106eadd057259
efb4518 <efb4518@ic.ac.uk>
2022-06-07 01:25:06 +0100

    Configure input channels to allow multiple inputs

--- image_to_label.py
+++ image_to_label.py
@@ -11,6 +11,9 @@ from tensorflow.keras.activations import tanh
 from tensorflow.keras import initializers
 from tensorflow.keras.metrics import MeanAbsoluteError
 import numpy as np
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.compat.v1.keras import backend
+import tensorflow as tf
 
 from data_loader import DataLoader
 from scene_gen_callback import SceneGenCallback
@@ -27,11 +30,20 @@ print("Num GPUs Available: ", len(config.list_physical_devices('GPU')))
 class ImageToLabel:
 
     def __init__(self, config):
-        self.filters = 4
-        self.kernel_size = 3
-        self.input_shape = (config.img_size, config.img_size, 3)
-        self.seg_shape = (config.img_size, config.img_size, 1)
-        self.depth_shape = (config.img_size, config.img_size, 1)
+        self.filters = config.filters
+        self.kernel_size = config.kernel_size
+
+        channels = 0
+        if config.use_rand:
+            channels += 3
+        if config.use_canon:
+            channels += 3
+        if config.use_depth:
+            channels += 1
+        if config.use_seg:
+            channels += 1
+
+        self.input_shape = (config.img_size, config.img_size, channels)
         self.conv_layers = 2
         self.initialiser = initializers.RandomNormal(stddev=0.02)
         self.opt = Adam()
@@ -39,18 +51,20 @@ class ImageToLabel:
         self.cnn = self.define_cnn()
         self.dataset_name = "/vol/bitbucket/efb4518/fyp/fyp/generated_imgs_numpy"
         # self.data_file_name = "RGB_peg_top_coords_in_image_array.npy"
-        self.data_loader = DataLoader(config=config)
-        self.test_data_loader = DataLoader(config=config)
+        self.data_loader = DataLoader(config=config, end=4000)
+        self.test_data_loader = DataLoader(config=config, start=4000)
 
-        self.use_canon = True
-        self.use_depth = True
-        self.use_seg = True
+        # self.use_canon = True
+        # self.use_depth = True
+        # self.use_seg = True
 
         print("data loaders: ", self.data_loader, self.test_data_loader)
-        self.data_loader.dataset_size=4500
+        # self.data_loader.dataset_size=4500
         self.data_loader.batch_size = 16
         self.model_save_interval = 1
         self.sample_interval=200
+        self.repetitions=config.repetitions
+        self.graph_name = config.graph_name
         
 
     def conv2d_layer(self, layer_inp, filters, stride=1):
@@ -65,21 +79,7 @@ class ImageToLabel:
         
         inp = Input(shape=self.input_shape)  # resize from img shape to rgb shape?
 
-        inp_seg = Input(shape=self.seg_shape)
-        inp_depth = Input(shape=self.depth_shape)
-
-        inputs_to_use = []
-
-        if self.use_canon:
-            inputs_to_use.append(inp)
-        if self.use_seg:
-            inputs_to_use.append(inp_seg)
-        if self.use_depth:
-            inputs_to_use.append(inp_depth)
-
-        combined_imgs = Concatenate(axis=-1)(inputs_to_use)
-
-        c = self.conv2d_layer(combined_imgs, self.filters, stride=1)
+        c = self.conv2d_layer(inp, self.filters, stride=1)
         for i in range(self.conv_layers):
             c = self.conv2d_layer(c, self.filters * 2 ** (i+1))
         c = Conv2D(self.filters*2** (self.conv_layers+1), kernel_size=self.kernel_size , activation='relu', 
@@ -98,89 +98,97 @@ class ImageToLabel:
 
         return model
 
+    def reinitialize(self, model):
+        for l in model.layers:
+            # session = backend.get_session()
+            if hasattr(l,"kernel_initializer"):
+                weights, biases = l.get_weights()
+                # weights_initializer = tf.variables_initializer(l.weights)
+                l.set_weights([l.kernel_initializer((tf.shape(weights))),
+                np.zeros(tf.shape(biases))
+                ])
+            else:
+                print("aaagggggaggggagag")
+                # session.run(weights_initializer)
+            # if hasattr(l,"bias_initializer"):
+            #     l.set_bias(l.bias_initializer(tf.shape(l.bias)))
+                # weights_initializer = tf.variables_initializer(l.biases)
+                # session.run(bias_initializer)
+            # if hasattr(l,"recurrent_initializer"):
+            #     l.recurrent_kernel.assign(l.recurrent_initializer(tf.shape(l.recurrent_kernel)))
+            # session.run(l.kernel_initializer)
+
     def train(self):
         start_time = datetime.datetime.now()
-
         
-        # test_data_loader.dataset_size = 1200
-        # test_data_generator = test_data_loader.load_batch_with_data(128)
+        min_losses= np.empty(self.repetitions)
+        min_val_losses = np.empty(self.repetitions)
 
-        # test_data = []
-        # test_vals = []
-        # for _ in range(8):
-        #     (imgs_A, target_vals) = next(test_data_generator)
-        #     test_data.append(imgs_A)
-        #     test_vals.append(target_vals)
+        losses = np.empty((self.repetitions, self.epochs))
+        val_losses = np.empty((self.repetitions, self.epochs))
 
-        # print(np.array(test_data).shape)
-        # print(np.array(test_vals).shape)
+        early_stopping_callback = EarlyStopping(monitor='val_loss', patience=2, restore_best_weights=True)
 
-        # test_data = np.reshape(np.array(test_data), (-1, self.input_shape[0], self.input_shape[1], self.input_shape[2]))
-        # test_vals = np.reshape(np.array(test_vals), (-1, 3))
+        for i in range(self.repetitions):
+            
+            # backend.clear_session()
+            # self.cnn.reset_states()
+            # self.reinitialize(self.cnn)
+            self.opt = Adam()
+            self.cnn = self.define_cnn()
+            
+            history = self.cnn.fit(x=self.data_loader, validation_data=self.test_data_loader, 
+                                    epochs=self.epochs, callbacks=[early_stopping_callback])
+                                    # , callbacks=[SceneGenCallback(sample_interval=self.sample_interval)])
+                                    # ,use_multiprocessing=True, workers=2)
 
-        test_losses= []
-        test_epochs = []
+            min_losses[i] = min(history.history['loss'])
+            min_val_losses[i] = min(history.history['val_loss'])
 
-        history = self.cnn.fit(x=self.data_loader, validation_data=self.test_data_loader, 
-                                epochs=self.epochs
-                                , callbacks=[SceneGenCallback(sample_interval=self.sample_interval)])
-                                # ,use_multiprocessing=True, workers=2)
+            losses[i, len(history.history['loss']):] = min_losses[i]
+            losses[i, :len(history.history['loss'])] = history.history['loss']
 
-        # for epoch in range(self.epochs):
-        #     for batch_i, (imgs_A, target_vals) in enumerate(self.data_loader.load_batch_with_data(self.batch_size)):
-        #         # train discriminator
+            val_losses[i, len(history.history['val_loss']):] = min_val_losses[i]
+            val_losses[i, :len(history.history['val_loss'])] = history.history['val_loss']
 
-        #         # print(target_vals)
 
-        #         # print(target_vals)
-
-        #         d_loss = self.cnn.train_on_batch(imgs_A, target_vals)
-
-        #         elapsed_time = datetime.datetime.now() - start_time
-        #         # Plot the progress
-        #         print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f, acc: %3d%%] time: %s" % (epoch,
-        #                                                                                  self.epochs,
-        #                                                                                  batch_i,
-        #                                                                                  self.data_loader.n_batches,
-        #                                                                                  d_loss[0],
-        #                                                                                  100 * d_loss[1],
-        #                                                                                  elapsed_time))
-
-        #         if batch_i % self.sample_interval == 0:
-        #             # plt.imshow(imgs_A[0])
-        #             # d_output = self.cnn(imgs_A.reshape(-1,256,256,3))[0]
-        #             # # print("d output before: ", d_output)
-        #             # d_output = self.image_to_screen(d_output[0], d_output[1], d_output[2])
-        #             # # print(d_output)
-        #             # # print("target vals before: ", target_vals[0])
-        #             # target_vals = self.image_to_screen(target_vals[0][0], target_vals[0][1], target_vals[0][2])
-        #             # # print(target_vals)
-        #             # # print("Target vals: ", target_vals)
-        #             # # print("d output: ", d_output)
-
-        #             # plt.plot(d_output[0], d_output[1], 'ro')
-        #             # plt.plot(target_vals[0], target_vals[1], 'go')
-        #             # plt.savefig("/vol/bitbucket/efb4518/fyp/fyp/models/fig_%d_%d" % (epoch, batch_i))
-        #             # # plt.show()
-        #             # plt.clf()
-
-        #             eval_results = self.cnn.evaluate(test_data, test_vals)
-        #             print(" \n EVAL RESULTS: \n    ", eval_results)
-        #             test_losses.append(eval_results[0])
-        #             # print("trainable: ", self.cnn.get_weights())
-
-        #         # if epoch % self.model_save_interval == 0 and batch_i == 0:
-        #         #     self.cnn.save("/vol/bitbucket/efb4518/fyp/fyp/models/model%d_%d" % (epoch, batch_i))
-
-        print(history)
-        plt.plot(history.history['loss'])
-        plt.plot(history.history['val_loss'])
-        plt.title('model loss')
+        # print(history)
+        plt.plot(np.mean(losses, axis=0))
+        plt.plot(np.mean(val_losses, axis=0))
+        plt.title('Mean model loss')
         plt.ylabel('loss')
         plt.xlabel('epoch')
         plt.legend(['train', 'test'], loc='upper left')
         # plt.show()
-        plt.savefig("/vol/bitbucket/efb4518/fyp/fyp/models/train_curve")
+        plt.savefig("/vol/bitbucket/efb4518/fyp/fyp/results/%s_mean"%self.graph_name)
+        plt.cla()
+        plt.clf()
+        plt.close()
+
+
+        plt.plot(np.median(losses, axis=0))
+        plt.plot(np.median(val_losses, axis=0))
+        plt.title('Median model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        # plt.show()
+        plt.savefig("/vol/bitbucket/efb4518/fyp/fyp/results/%s_median"%self.graph_name)
+
+        with open('/vol/bitbucket/efb4518/fyp/fyp/results/%s.txt'%self.graph_name, 'w') as f:
+            f.write("losses: " +np.array2string(losses) +
+                    "\nval losses: " +np.array2string(val_losses) +
+                    "\nmean losses: "+ np.array2string(np.mean(losses, axis=0))+
+                    "\nmean val losses: " + np.array2string(np.mean(val_losses, axis=0))+
+                    "\nmedian losses: "+ np.array2string(np.median(losses, axis=0))+
+                    "\nmedian val losses: " + np.array2string(np.median(val_losses, axis=0))+
+                    "\nmean min loss: " + np.array2string(np.mean(min_losses))+
+                    "\nmedian min loss: " + np.array2string(np.median(min_losses))+
+                    "\nmin min loss: " + np.array2string(np.min(min_losses))+
+                    "\nmean min val loss: " + np.array2string(np.mean(min_val_losses))+
+                    "\nmedian min val loss: " + np.array2string(np.median(min_val_losses))+
+                    "\nmin min val loss: " +np.array2string(np.min(min_val_losses)))
+
 
 # if __name__ == "__main__":
 #     cnn = ImageToLabel()
